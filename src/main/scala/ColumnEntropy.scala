f1dd3ad6e6648d1518f36c084695918c1794fcdd
Liang-Chi Hsieh <viirya@gmail.com>
2014-10-01 23:55:33 +0800

    remove the unnecessary column used for counting.

--- src/main/scala/ColumnEntropy.scala
+++ src/main/scala/ColumnEntropy.scala
@@ -11,21 +11,20 @@ import scala.collection.{Map => Map}
 object ColumnEntropy {
 
   def main(args: Array[String]) {
-    if (args.length < 4) {
-      System.err.println("Usage: ColumnEntropy <column1> <column2> <count column> <table>")
+    if (args.length < 3) {
+      System.err.println("Usage: ColumnEntropy <column1> <column2> <table>")
       System.exit(1)
     }
 
     val first_column_name = args(0)
     val second_column_name = args(1)
-    val count_column_name = args(2)
-    val table = args(3)
+    val table = args(2)
 
     val conf = new SparkConf().setAppName("Calculate column entropy")
     val sc = new SparkContext(conf)
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
 
-    val queries = Map(first_column_name -> s"select cast($first_column_name as STRING), count($count_column_name) from $table group by $first_column_name", second_column_name -> s"select cast($second_column_name as STRING), count($count_column_name) from $table group by $second_column_name")
+    val queries = Map(first_column_name -> s"select cast($first_column_name as STRING), count(cast($first_column_name as STRING)) from $table group by $first_column_name", second_column_name -> s"select cast($second_column_name as STRING), count(cast($second_column_name as STRING)) from $table group by $second_column_name")
 
     var results = Map[String, Double]() 
 
@@ -49,7 +48,7 @@ object ColumnEntropy {
 
     // mutual information
 
-    val query = s"select cast($first_column_name as STRING), cast($second_column_name as STRING), count($count_column_name) from $table group by $first_column_name, $second_column_name"
+    val query = s"select cast($first_column_name as STRING), cast($second_column_name as STRING), count(cast($second_column_name as STRING)) from $table group by $first_column_name, $second_column_name"
 
     val counts = hiveContext.hql(query).map(r => { (r.getString(0), r.getString(1), r.getLong(2))}).collect()
     var total = 0L
